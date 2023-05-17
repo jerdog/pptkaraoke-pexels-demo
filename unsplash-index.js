39f2ceed61e4e96afda364ddca6d360c8e151e43
Jeremy Meiss <jeremy.meiss@gmail.com>
2023-05-17 12:07:55 -0500

    messing around with the scripts

--- unsplash-index.js
+++ unsplash-index.js
@@ -1,8 +1,9 @@
 // index.js - taken from https://javascript.plainenglish.io/a-beginners-guide-to-unsplash-api-in-javascript-2524c51ae1f3
 
-import { Unsplash } from "./unsplash.js";
+import { Unsplash } from "./unsplash.mjs";
 import dotenv from 'dotenv';
 dotenv.config();
 
 const unsplash = new Unsplash(process.env.UNSPLASH_KEY);
+//await unsplash.getPhoto('file', 'coding on laptop');
 await unsplash.getPhoto('file', 'coding on laptop');
