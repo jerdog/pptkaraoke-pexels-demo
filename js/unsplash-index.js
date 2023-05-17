d353e49460af74ddd8ba512d1ca7408a94bba36b
Jeremy Meiss <jeremy.meiss@gmail.com>
2023-05-17 11:26:10 -0500

    move js files and rename

---
+++ js/unsplash-index.js
@@ -0,0 +1,8 @@
+// index.js - taken from https://javascript.plainenglish.io/a-beginners-guide-to-unsplash-api-in-javascript-2524c51ae1f3
+
+import { Unsplash } from "./unsplash.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const unsplash = new Unsplash(process.env.UNSPLASH_KEY);
+await unsplash.getPhoto('file', 'coding on laptop');
