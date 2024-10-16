431489d598a4bd1123d5bda8b5b9cb20bb293606
Jeremy Meiss <jeremy.meiss@gmail.com>
2024-10-15 21:31:46 -0500

    cleanup

--- .netlify/functions/unsplash.js
+++ .netlify/functions/unsplash.js
@@ -1,26 +1,5 @@
 // netlify/functions/unsplash.js
 
-//const fetch = require('node-fetch'); // You can use fetch in Node.js to make HTTP requests
-
-// exports.handler = async function (event, context) {
-//   const ACCESS_KEY = process.env.UNSPLASH_KEY; // Use environment variables for security
-
-//   try {
-//     const response = await fetch(`https://api.unsplash.com/photos/random?query=funny+photos&orientation=landscape&content_filter=low&client_id=${ACCESS_KEY}`);
-//     const data = await response.json();
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(data)
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: 'Error fetching Unsplash data' })
-//     };
-//   }
-// };
-
 exports.handler = async function(event, context) {
   try {
     // Use dynamic import for ES Module
